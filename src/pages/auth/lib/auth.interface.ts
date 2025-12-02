18e90903df41582757c6b08177508537d3b18432
david3010M <hvaldiviezos@unprg.edu.pe>
2025-12-01 20:19:25 -0500

    Update auth.interface.ts

--- src/pages/auth/lib/auth.interface.ts
+++ src/pages/auth/lib/auth.interface.ts
@@ -14,22 +14,29 @@ export interface User {
   person: Person;
   rol_id: number;
   rol_name: string;
+  boxes: Box[];
 }
 
 export interface Person {
   id: number;
-  type_document: string;
-  type_person: string;
   number_document: string;
+  type_person: string;
   names: string;
   father_surname: string;
   mother_surname: string;
-  business_name: string;
-  address: string;
+  gender?: string;
+  birth_date?: string;
   phone: string;
   email: string;
-  ocupation: string;
-  status: string;
+  address: string;
+  business_name: string;
+  commercial_name: string;
+  occupation: string;
+  job_position_id?: string;
+  business_type_id?: string;
+  zone_id?: string;
+  created_at: string;
+  document_type_id?: string;
 }
 
 export interface Access {
@@ -46,4 +53,14 @@ export interface Permission {
   actions: string[];
   routes: string[];
   status: string;
+}
+
+export interface Box {
+  id: number;
+  name: string;
+  status: string;
+  serie: string;
+  branch_id: number;
+  branch_name: string;
+  created_at: string;
 }
